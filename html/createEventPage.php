d29437c1747024dd4634cf1ffb436354a2a192e6
Yeejkoob Thao <ythao1@student.gsu.edu>
2017-04-11 14:19:55 -0400

    initial setup of all the form data

--- html/createEventPage.php
+++ html/createEventPage.php
@@ -36,7 +36,7 @@
  * Time: 11:35 PM
  */
 // Setup the connection info and connect - Keep the connectDB.php file off of Github
-require 'connectDB.php';
+//require 'connectDB.php';
 
 ?>
 <body bgcolor="#AFEBBC">
@@ -46,12 +46,70 @@ require 'connectDB.php';
 <div class="container">
     <div class="row">
         <div class="one-half column">
-            <form>
-                Event Name:<br>
-                <input type="text" name="eventName"><br>
+            <form id="eventCreationForm" method="post" action="createEventPage.php">
+                Event Name: <input type="text" name="eventName"><br>
                 Event Description:<br>
-                <input type="text" name="eventDescription"><br>
-                <input type="submit">
+                <textarea name="eventDescription"></textarea><br>
+                Event Time Begins: <input type="date" name="eventTimeBegins"><br>
+                Event Time Ends: <input type="date" name="eventTimeEnds"><br>
+                Event Country: <input type="text" name="eventCountry"><br>
+                Event Address: <input type="text" name="eventAddress"><br>
+                Event State:
+                <select name="eventState">
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                </select><br>
+                Event City: <input type="text" name="eventState"><br>
+                <input type="submit" name="submitButton">
             </form>
         </div>
     </div>
