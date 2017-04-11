34fc6c3473a57c20a37b4b734942b344ad1c837e
Yeejkoob Thao <ythao1@student.gsu.edu>
2017-04-11 14:00:27 -0400

    Setting up the initial form data

--- html/createEventPage.php
+++ html/createEventPage.php
@@ -1,10 +1,13 @@
+<!--
+    This page is a form for when users want to create an event on vive-les-sept
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <!-- Basic Page Needs
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
-    <title>Vive Les Sept</title>
+    <title>Vive Les Sept - Create Event</title>
     <meta name="description" content="A small social media web application">
     <meta name="author" content="Vive Les Sept">
 
@@ -20,20 +23,36 @@
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <link rel="stylesheet" href=".././css/normalize.css">
     <link rel="stylesheet" href=".././css/skeleton.css">
-
     <!-- Favicon
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <link rel="icon" type="image/png" href="images/favicon.png">
 
 </head>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yeejkoob
+ * Date: 4/10/2017
+ * Time: 11:35 PM
+ */
+// Setup the connection info and connect - Keep the connectDB.php file off of Github
+require 'connectDB.php';
+
+?>
 <body bgcolor="#AFEBBC">
 
 <!-- Primary Page Layout
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <div class="container">
     <div class="row">
-        <div class="one-half column" style="position: absolute; top: 0px; right: 0px;" >
-            <p>Place holder for the newsfeed content and layout</p>
+        <div class="one-half column">
+            <form>
+                Event Name:<br>
+                <input type="text" name="eventName"><br>
+                Event Description:<br>
+                <input type="text" name="eventDescription"><br>
+                <input type="submit">
+            </form>
         </div>
     </div>
 </div>
