c58e61890ae3f75d0025c0f3a351c94d6109b513
Yeejkoob Thao <ythao1@student.gsu.edu>
2017-04-12 12:20:49 -0400

    Separated the business logic php into its own directory

---
+++ php/insertEventData.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yeejkoob
+ * Date: 4/10/2017
+ * Time: 11:35 PM
+ */
+// Setup the connection info and connect - Keep the connectDB.php file off of Github
+require 'connectDB.php';
+// define variables and set to empty values
+$hostUserName = $_SESSION["userName"]; // Assume that the username has been put into a userName Session Key
+$hostId = "";
+$eventName = "";
+$eventDescription = "";
+$eventTimeBegins = "";
+$eventTimeEnd = "";
+$eventCountry = "";
+$eventAddress = "";
+$eventState = "";
+$eventCity = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $eventName = normalizeData($_POST["eventName"]);
+    $eventDescription = normalizeData($_POST["eventDescription"]);
+    $eventTimeBegins = normalizeData($_POST["eventTimeBegins"]);
+    $eventTimeEnd = normalizeData($_POST["eventTimeEnds"]);
+    $eventCountry = normalizeData($_POST["eventCountry"]);
+    $eventAddress = normalizeData($_POST["eventAddress"]);
+    $eventState = normalizeData($_POST["eventState"]);
+    $eventCity = normalizeData($_POST["eventCity"]);
+
+    // Get the user's id and use that as the poster's id
+    $sql = "SELECT users.user_id FROM csc4710team7 AS users WHERE users.username=$hostUserName";
+    $result = query($mysqli, $sql);
+    $row = $result->fetch_assoc();
+    $hostId = $row["user_id"];
+
+    // Insert the event data into the DB
+    $sql = "INSERT INTO csc4710team7.tbl_event " .
+        "(host_id, name, description, country, city, state, address, time_begins, time_ends) " .
+        "VALUES($hostID, 
+                $eventName, 
+                $eventDescription, 
+                $eventCountry, 
+                $eventCity, 
+                $eventAddress, 
+                $eventTimeBegins, 
+                $eventTimeEnd)";
+    $result = query($mysqli, $sql);
+    echo "Inserted the event into tbl_event<br>";
+}
+
+function normalizeData($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
